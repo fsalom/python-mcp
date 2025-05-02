34b29ede04a8650fc695dc9445652608d6acc9e8
Fernando Salom <fer@rudo.es>
2025-05-02 13:31:35 +0200

    Remove containers

--- driving/api_rest/containers.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-from driving.api_rest.v1.bitbucket.adapter import BitbucketContainer
-
-
-def add_containers(app: FastAPI):
-    bitbucket_container = BitbucketContainer()
-    app.bitbucket_container = bitbucket_container
