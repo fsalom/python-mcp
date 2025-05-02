3adb90fe8c5482ae6cb2af00819d70738856944c
Fernando Salom <fer@rudo.es>
2025-05-02 15:59:37 +0200

    Add coin mapper for repository

---
+++ driven/api_rest/v1/coin/mapper.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from domain.coin import Coin
+from driven.api_rest.v1.coin.models import CoinDataResponse
+
+
+class CoinMapper:
+
+    @staticmethod
+    def to_domains(data: CoinDataResponse) -> List[Coin]:
+        return [
+            Coin(
+                id=coin.id,
+                symbol=coin.symbol,
+                name=coin.name,
+                price_usd=coin.price_usd,
+                market_cap_usd=coin.market_cap_usd,
+                change_percent_24hr=coin.change_percent_24hr,
+            )
+            for coin in data.data
+        ]
