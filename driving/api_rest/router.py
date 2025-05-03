2edd77d8f80803a91c487a99aa427f4735ddf813
Fernando Salom <fer@rudo.es>
2025-05-03 18:32:37 +0200

    Add router for ai

--- driving/api_rest/router.py
+++ driving/api_rest/router.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 
 from driving.api_rest.health.adapter import health_router
-from driving.api_rest.v1.authentication.adapter import auth_router
-from driving.api_rest.v1.tickets.adapter import ticket_router
-from driving.api_rest.v1.user.adapter import user_router
+from driving.api_rest.v1.ai.adapter import ai_router
 
 
 def add_routers(app: FastAPI):
     app.include_router(health_router, prefix='/api')
     # v1
+    app.include_router(ai_router, prefix='/api/v1')
