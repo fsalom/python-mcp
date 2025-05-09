a7a84c02768058aa680178d34893d2e8b084026f
Fernando Salom <fer@rudo.es>
2025-05-09 12:19:28 +0200

    Add detail for crypto

--- driven/api_rest/v1/coin/adapter.py
+++ driven/api_rest/v1/coin/adapter.py
@@ -5,7 +5,7 @@ import httpx
 from application.ports.driven.coin_repository_port import CoinRepositoryPort
 from domain.coin import Coin
 from driven.api_rest.v1.coin.mapper import CoinMapper
-from driven.api_rest.v1.coin.models import CoinDataResponse
+from driven.api_rest.v1.coin.models import CoinDataResponse, CoinData, CoinDataSingleResponse
 
 
 class CoinRepositoryAdapter(CoinRepositoryPort):
@@ -19,3 +19,11 @@ class CoinRepositoryAdapter(CoinRepositoryPort):
 
             data = CoinDataResponse(**response.json())
             return self.mapper.to_domains(data)
+
+    async def get_coin_with_id(self, id) -> Coin:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"https://rest.coincap.io/v3/assets/{id}/?apiKey={os.getenv('API_KEY')}")
+            response.raise_for_status()
+
+            info = CoinDataSingleResponse(**response.json())
+            return self.mapper.to_domain(info.data)
