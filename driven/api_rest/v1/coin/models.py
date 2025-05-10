b726ffe7bc9c2c821ae25e9905b4687d1ebaa73b
Fernando Salom <fer@rudo.es>
2025-05-10 10:24:50 +0200

    Add dtos models

--- driven/api_rest/v1/coin/models.py
+++ driven/api_rest/v1/coin/models.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from pydantic import BaseModel
+from okx import MarketData
+from pydantic import BaseModel, Field
 
 
 class CoinData(BaseModel):
@@ -33,3 +34,34 @@ class CoinDataSingleResponse(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class MarketDataResponse(BaseModel):
+    data: List[MarketData]
+    timestamp: int
+
+
+class MarketData(BaseModel):
+    exchange_id: str = Field(alias="exchangeId")
+    base_id: str = Field(alias="baseId")
+    quote_id: str = Field(alias="quoteId")
+    base_symbol: str = Field(alias="baseSymbol")
+    quote_symbol: str = Field(alias="quoteSymbol")
+    volume_usd_24hr: float = Field(alias="volumeUsd24Hr")
+    price_usd: float = Field(alias="priceUsd")
+    volume_percent: float = Field(alias="volumePercent")
+
+    class Config:
+        allow_population_by_field_name = True
+        populate_by_name = True
+        from_attributes = True
+
+
+class HistoricalPriceData(BaseModel):
+    price_usd: float = Field(alias="priceUsd")
+    date: str
+
+
+class HistoricalData(BaseModel):
+    data: List[HistoricalPriceData]
+    timestamp: int
