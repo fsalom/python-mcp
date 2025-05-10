a4d26f94013a178f0b6eff1321164d6d51334630
Fernando Salom <fer@rudo.es>
2025-05-10 10:24:37 +0200

    Add mapper for new functionality

--- application/ports/driven/coin_repository_port.py
+++ application/ports/driven/coin_repository_port.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod, ABC
 from typing import List
 from domain.coin import Coin
+from domain.market import Market
 
 
 class CoinRepositoryPort(ABC):
@@ -10,4 +11,8 @@ class CoinRepositoryPort(ABC):
 
     @abstractmethod
     def get_coin_with_id(self, id) -> Coin:
+        pass
+
+    @abstractmethod
+    def get_markets_for_coin_with_id(self, id) -> List[Market]:
         pass
