d42958a7fe4d73e937f968329d14c633c2a391dd
Fernando Salom <fer@rudo.es>
2025-05-10 10:24:57 +0200

    Update mapper.py

--- driven/api_rest/v1/coin/mapper.py
+++ driven/api_rest/v1/coin/mapper.py
@@ -1,7 +1,10 @@
 from typing import List
 
+from domain import market
 from domain.coin import Coin
-from driven.api_rest.v1.coin.models import CoinDataResponse, CoinData
+from domain.historical_price import HistoricalPrice
+from domain.market import Market
+from driven.api_rest.v1.coin.models import CoinDataResponse, CoinData, MarketDataResponse, HistoricalPriceData
 
 
 class CoinMapper:
@@ -24,3 +27,29 @@ class CoinMapper:
                     name=coin.name,
                     price_usd=coin.price_usd,
                     market_cap_usd=coin.market_cap_usd)
+
+    @staticmethod
+    def to_domains_market(data: MarketDataResponse) -> List[Market]:
+        return [
+            Market(
+                exchange_id=market.exchange_id,
+                base_id=market.base_id,
+                quote_id=market.quote_id,
+                base_symbol=market.base_symbol,
+                quote_symbol=market.quote_symbol,
+                volume_usd_24hr=market.volume_usd_24hr,
+                price_usd=market.price_usd,
+                volume_percent=market.volume_percent
+            )
+            for market in data.data
+        ]
+
+    @staticmethod
+    def to_domains_historical_price(data: HistoricalPriceData) -> List[HistoricalPrice]:
+        return [
+            HistoricalPrice(
+                price_usd=price.price_usd,
+                date=price.date
+            )
+            for price in data.data
+        ]
