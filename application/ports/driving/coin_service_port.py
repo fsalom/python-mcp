a3ac896e1e866a8bdc0e265e43862d0e43ee521c
Fernando Salom <fer@rudo.es>
2025-05-12 10:49:26 +0200

    Add new functionality to get crypto by id, prices in exchanges and historical price

--- application/ports/driving/coin_service_port.py
+++ application/ports/driving/coin_service_port.py
@@ -1,10 +1,30 @@
 from abc import abstractmethod, ABC
-from typing import List
+from typing import List, Optional
 
 from domain.coin import Coin
+from domain.historical_price import HistoricalPrice
+from domain.market import Market
 
 
 class CoinServicePort(ABC):
     @abstractmethod
     def get_coins(self) -> List[Coin]:
+        pass
+
+    @abstractmethod
+    def get_coin_with_id(self, id) -> Coin:
+        pass
+
+    @abstractmethod
+    def get_markets_for_coin_with_id(self, id) -> List[Market]:
+        pass
+
+    @abstractmethod
+    def get_historical_price_for_coin_with_id(
+            self,
+            id: str,
+            interval: str,
+            start: Optional[int] = None,
+            end: Optional[int] = None
+    ) -> List[HistoricalPrice]:
         pass
