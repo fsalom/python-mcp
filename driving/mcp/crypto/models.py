31de4391ca1dd50ef7d70d9e03c55dacf29eebd1
Fernando Salom <fer@rudo.es>
2025-05-12 10:50:20 +0200

    Add request object for historical price request

--- driving/mcp/crypto/models.py
+++ driving/mcp/crypto/models.py
@@ -1,5 +1,14 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class CoinIdRequest(BaseModel):
     id: str
+
+
+class HistoricalPriceRequest(BaseModel):
+    id: str
+    interval: str
+    start: Optional[int] = None
+    end: Optional[int] = None
